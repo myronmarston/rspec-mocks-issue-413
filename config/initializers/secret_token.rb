a92c5cfe996e2df08984905a4f3e636160972797
Myron Marston <myron.marston@gmail.com>
2013-08-26 08:08:46 -0700

    Skeleton.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RspecMocks413::Application.config.secret_token = '8e753ddf66d70251d574f5c2036a63fe7fb449d6b519b6ad7ce063cd2c9c8049b6fd3ebcaaef8d466f0ad713757141058650558d4133544e636c22a695178296'
